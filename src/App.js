824e1f1f733b78bb86762769a92cbfa8320a0861
Gregorius Agung Narindra Aditantyo <narindra.aditantyo@gmail.com>
2020-10-08 17:12:35 +0700

    Tugas 9 - Pengenalan Reactjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<form>
+				<h1>Form Pembelian Buah</h1>
+				<ul className="FormContainer">
+					<li className="FormList">
+						<label>Nama Pelanggan</label>
+						<input type="text" />
+					</li>
+					<li className="FormList">
+						<label>Daftar Item</label>
+						<div className="CheckboxList">
+							<ul className="CheckboxGroup">
+								<li>
+									<input type="checkbox" name="buah" />
+									<label>Semangka</label>
+								</li>
+								<li>
+									<input type="checkbox" name="buah" />
+									<label>Jeruk</label>
+								</li>
+								<li>
+									<input type="checkbox" name="buah" />
+									<label>Nanas</label>
+								</li>
+								<li>
+									<input type="checkbox" name="buah" />
+									<label>Salak</label>
+								</li>
+								<li>
+									<input type="checkbox" name="buah" />
+									<label>Anggur</label>
+								</li>
+							</ul>
+						</div>
+					</li>
+					<li className="FormList">
+						<button type="submit">Kirim</button>
+					</li>
+				</ul>
+			</form>
+		</div>
+	);
 }
 
 export default App;
